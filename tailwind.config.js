67288723f1e12a1ea47656496e64a80f95547b82
Gabriel Alves <l.luis.gabriel@outlook.com>
2021-08-30 17:02:01 -0300

    feat(spotify): add spotify circular fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xxs: ['0.688rem', '1rem'],
+      },
+      fontFamily: {
+        spotify: ['spotify-circular', 'sans-serif'],
+      },
+      spacing: {
+        15: '3.75rem',
+        34: '8.5rem',
+      },
+      colors: {
+        spotify: {
+          black: '#000000',
+          900: '#121212',
+          800: '#181818',
+          700: '#282828',
+          600: '#686868',
+          500: '#b2b2b2',
+          white: '#ffffff',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
